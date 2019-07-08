e4b70ee0cad6d7b803183ad85de20fd115a79351
Kishor Pandey <kishor.jpandey@gmail.com>
2019-07-08 11:25:15 -0700

    remove

--- taskmaster-frontend/src/app.js
+++ taskmaster-frontend/src/app.js
@@ -6,38 +6,42 @@ import Header from './header'
 const API ='http://task.us-west-2.elasticbeanstalk.com/task'
 
 function Task(){
+
   const [task, setTask] = useState([]);
-  const getTask = () =>{
+
+  const _getTask = () => {
     fetch(API, {
-     mode:'cors',
+    mode:'cors',
     })
-    .then(data => data.json() )
-    .then(task => setTask(task) )
+    .then( data => data.json() )
+    .then( tsk => setTask(tsk) )
     .catch(console.error);
 
   };
-  useEffect(getTask, []);
 
-  return(<ul>
+  useEffect(_getTask, []);
 
-    {task.map((list)=>
-      <li key={list.id}>
-      <p>Task:{list.title}</p>
-      <p>Description: {list.description}</p>
-      <p>Status: {list.status}</p>
-      <p>Assignee: {list.assignee}</p>
-
-      </li>
-      )}
-  </ul>)
-
+  return(
+      <ul>
+        {task.map((list) =>
+          <li key={list.id}>
+              <p>Task:{list.title}</p>
+              <p>Description: {list.description}</p>
+              <p>Status: {list.status}</p>
+              <p>Assignee: {list.assignee}</p>
+          </li>
+          )}
+      </ul>
+  )
 }
+
 function App() {
   return (
     <>
     <Header />
-
+    <main>
         <Task />
+    </main>
 
     </>
   );
